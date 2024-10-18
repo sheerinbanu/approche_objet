997110f999083ad457e9f643a94942afb5e9e35e
Sheerin Banu <sheemuzz@outlook.com>
2024-10-18 17:05:28 +0200

    TP 7

---
+++ src/fr/diginamic/banque/Compte.java
@@ -0,0 +1,24 @@
+package fr.diginamic.banque;
+
+public class Compte {
+    public long numeroCompte;
+
+    private double solde;
+
+    public Compte(long numeroCompte, double solde) {
+        this.numeroCompte = numeroCompte;
+        solde = 0;
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+
+    @Override
+    public String toString() {
+        return "Compte{" +
+                "numeroCompte=" + numeroCompte +
+                ", solde=" + solde +
+                '}';
+    }
+}
---
+++ src/fr/diginamic/banque/CompteTaux.java
@@ -0,0 +1,27 @@
+package fr.diginamic.banque;
+
+public class CompteTaux extends Compte{
+
+    private double tauxDeRemuneration;
+
+    public CompteTaux(long numeroCompte, double solde, double tauxDeRemuneration) {
+        super(numeroCompte, solde);
+        this.tauxDeRemuneration = tauxDeRemuneration;
+    }
+
+    public double getTauxDeRemuneration() {
+        return tauxDeRemuneration;
+    }
+
+    public void setTauxDeRemuneration(double tauxDeRemuneration) {
+        this.tauxDeRemuneration = tauxDeRemuneration;
+    }
+
+    @Override
+    public String toString() {
+        return "CompteTaux{" +
+                "tauxDeRemuneration=" + tauxDeRemuneration +
+                ", numeroCompte=" + numeroCompte +
+                '}';
+    }
+}
