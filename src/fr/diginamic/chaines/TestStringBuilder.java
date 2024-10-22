9696d6c05ae403f12bc02d88fc09305b3ccaedf5
Sheerin Banu <sheemuzz@outlook.com>
2024-10-22 14:52:44 +0200

    TP StringBuilder

--- src/fr/diginamic/chaines/TestStringBuilder.java
+++ src/fr/diginamic/chaines/TestStringBuilder.java
@@ -10,5 +10,14 @@ public class TestStringBuilder {
         System.out.println(s.toString());
         long fin = System.currentTimeMillis();
         System.out.println("Temps écoulé en millisecondes : " + (fin-debut));
+
+        long debut2 = System.currentTimeMillis();
+        String s2 = "";
+        for (int i = 0; i <=100000; i++) {
+            s2+=i;
+        }
+        System.out.println(s2.toString());
+        long fin2 = System.currentTimeMillis();
+        System.out.println("Temps écoulé en millisecondes : " + (fin2-debut2));
     }
 }
