a4796a797adc14c4f2b33917adfbfd37e12089b4
Sheerin Banu <sheemuzz@outlook.com>
2024-10-24 11:15:40 +0200

    TP 14 TRI EXO4 correction comparatorHabitant

--- src/fr/diginamic/listes/ComparatorHabitant.java
+++ src/fr/diginamic/listes/ComparatorHabitant.java
@@ -9,9 +9,9 @@ public class ComparatorHabitant implements Comparator<Ville> {
     @Override
     public int compare(Ville v1, Ville v2) {
         if(v1.getNbHab()>v2.getNbHab()){
-            return -1;
-        } else if (v1.getNbHab()<v2.getNbHab()) {
             return 1;
+        } else if (v1.getNbHab()<v2.getNbHab()) {
+            return -1;
         } else  {
             return 0;
         }
